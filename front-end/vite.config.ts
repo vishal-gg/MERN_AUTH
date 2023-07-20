83f5c6d1a950d78f8685177b37fd3f9d1b22250c
vishal <121763346+vishal-gg@users.noreply.github.com>
2023-07-21 00:47:30 +0530

    added vercel.json file and edited proxy

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://mern-auth-serverside.vercel.app',
         changeOrigin: true
       }
     }
